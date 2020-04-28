cab186133577ce1cd01b19c2579e59aa34f58c1b
ryandougc <ryan.carr@emailkpu.ca>
2020-04-28 00:18:56 -0700

    Testing connecting 2 users

--- server.test.js
+++ server.test.js
@@ -1,19 +1,52 @@
 const puppeteer = require('puppeteer')
 
-test('connect-user', async () => {
+// test('create game', async () => {
+//     const browser = await puppeteer.launch({
+//         headless: false
+//         ,args: ['--window-size=1280,720', '--auto-open-devtools-for-tabs']
+//     })
+
+
+//         const page = await browser.newPage()
+//         await page.goto('http://localhost:3000/')
+
+//         await page.click('input#inputGameName')
+//         await page.type('input#inputGameName', 'test-game')
+//         await page.click('button#submitNewGame')
+//         await page.on('dialog', async dialog => {
+//             await dialog.accept(`puppet`)
+//         })
+
+//         const page2 = await browser.newPage()
+//         await page2.goto('http://localhost:3000/')
+//         await page2.click('a#joinGameLink')
+//         await page2.on('dialog', dialog => {
+//             dialog.accept(`puppet2`)
+//         })
+// }, 25000)
+
+test('reconnect-user', async () => {
     const browser = await puppeteer.launch({
         headless: false
-        ,slowMo: 90
+        ,slowMo: 99
         ,args: ['--window-size=1280,720', '--auto-open-devtools-for-tabs']
     })
-        const page = await browser.newPage()
-        await page.goto('http://localhost:3000/')
 
         //Init player 1
+        const page = await browser.newPage()
+        await page.goto('http://localhost:3000/')
+        await page.click('input#inputName')
+        await page.type('input#inputName', 'puppet')
+        await page.click('button#nameInputSubmit')
         await page.click('input#inputGameName')
         await page.type('input#inputGameName', 'test-game')
         await page.click('button#submitNewGame')
-        await page.on('dialog', async dialog => {
-            await dialog.accept(`puppet`)
-        })
-})
+
+        //Init player 2
+        const page2 = await browser.newPage()
+        await page2.goto('http://localhost:3000/')
+        await page2.click('input#inputName')
+        await page2.type('input#inputName', 'puppet2')
+        await page2.click('button#nameInputSubmit')
+        await page2.click('a#joinGameLink')
+}, 25000)
